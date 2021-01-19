19da721c2d8188103552d39b4d9bc878acdcc6b0
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-01-19 13:38:45 -0600

    Convert given video path to HLS

--- ffmpeg.go
+++ ffmpeg.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+	"path"
+
+	"github.com/google/uuid"
+	"github.com/spf13/viper"
 )
 
 // HLSChunkLength - Size of HLS pieces in seconds
 const HLSChunkLength = 10
 
 func convertToHLS(videoFile string) string {
-	videoFolder := "/home/nesbitt/test"
-	exec.Command("ffmpeg -i \"" + videoFile + "\" -profile:v baseline -level 3.0 -s 1920x1080 -start_number 0 -hls_time " + fmt.Sprint(HLSChunkLength) + " -hls_list_size 0 -f hls \"" + videoFolder + "/master.m3u8\"")
+	videoFolder := path.Join(viper.GetString("videoStorageFolder"), uuid.New().String())
+
+	// TODO: Use proper FileMode
+	err := os.Mkdir(videoFolder, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmd := exec.Command(viper.GetString("ffmpegDir"), "-i", videoFile, "-profile:v", "baseline", "-level", "3.0", "-s", "1920x1080", "-start_number", "0", "-hls_time", fmt.Sprint(HLSChunkLength), "-hls_list_size", "0", "-f", "hls", path.Join(videoFolder, "/master.m3u8"))
+
+	fmt.Printf("Converting %s to HLS\n", videoFile)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		log.Fatal(err)
+	}
 
 	return videoFolder
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/gatsby-tv/dapper-cli
 go 1.15
 
 require (
+	github.com/google/uuid v1.1.5
 	github.com/ipfs/go-ipfs v0.7.0
 	github.com/ipfs/go-ipfs-config v0.9.0
 	github.com/ipfs/go-ipfs-files v0.0.8
@@ -10,5 +11,5 @@ require (
 	github.com/libp2p/go-libp2p-core v0.6.1
 	github.com/libp2p/go-libp2p-peerstore v0.2.6
 	github.com/multiformats/go-multiaddr v0.3.1
-	github.com/spf13/viper v1.7.1 // indirect
+	github.com/spf13/viper v1.7.1
 )

5edbfd2fe51f1f5582092b2387c6ef4f48947048
Michael Nesbitt <nesbitt@treeman.nesbitt.rocks>
2021-02-03 10:22:52 -0600

    Moved getting video length to separate function

--- ffmpeg.go
+++ ffmpeg.go
@@ -15,41 +15,46 @@ import (
 // HLSChunkLength - Size of HLS pieces in seconds
 const HLSChunkLength = 10
 
-// TODO: Break into separate functions (getVideoLength)
-func convertToHLS(videoFile string) (videoFolder string, videoLength int, err error) {
+func getVideoLength(videoFile string) (videoLength int, err error) {
 	// Get length of video
-	cmd := exec.Command(viper.GetString("ffprobeDir"), "-i", videoFile, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=\"p=0\"")
-	out, err := cmd.Output()
+	cmd := exec.Command(viper.GetString("ffprobeDir"), "-i", videoFile, "-show_entries", "format=duration", "-v", "quiet", "-of", `csv=p=0`)
+	out, err := cmd.CombinedOutput()
 	if err != nil {
-		return "", 0, err
+		fmt.Println(string(out))
+		return 0, err
 	}
 
-	videoLengthFloat, err := strconv.ParseFloat(string(out), 64)
+	videoLengthFloat, err := strconv.ParseFloat(string(out[:len(out)-1]), 64)
 	if err != nil {
-		return "", 0, err
+		return 0, err
 	}
 
 	videoLength = int(math.Ceil(videoLengthFloat))
 
+	return videoLength, nil
+}
+
+// TODO: Break into separate functions (getVideoLength)
+func convertToHLS(videoFile string) (videoFolder string, err error) {
 	// Create folder to store HLS video in
 	videoFolder = path.Join(viper.GetString("videoStorageFolder"), uuid.New().String())
 	// TODO: Use proper FileMode
 	err = os.Mkdir(videoFolder, 0755)
 	if err != nil {
-		return "", 0, err
+		return "", err
 	}
 
 	// Convert video
 	// TODO: Move to separate thread
 	// TODO: Create way of checking status of transcode
-	cmd = exec.Command(viper.GetString("ffmpegDir"), "-i", videoFile, "-profile:v", "baseline", "-level", "3.0", "-s", "1920x1080", "-start_number", "0", "-hls_time", fmt.Sprint(HLSChunkLength), "-hls_list_size", "0", "-f", "hls", path.Join(videoFolder, "/master.m3u8"))
+	cmd := exec.Command(viper.GetString("ffmpegDir"), "-i", videoFile, "-profile:v", "baseline", "-level", "3.0", "-s", "1920x1080", "-start_number", "0", "-hls_time", fmt.Sprint(HLSChunkLength), "-hls_list_size", "0", "-f", "hls", path.Join(videoFolder, "/master.m3u8"))
 
 	fmt.Printf("Converting %s to HLS\n", videoFile)
-	out, err = cmd.CombinedOutput()
+	out, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println(string(out))
-		return "", 0, err
+		return "", err
 	}
 
-	return videoFolder, videoLength, nil
+	return videoFolder, nil
 }
